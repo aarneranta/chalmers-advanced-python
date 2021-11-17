7fe8a5529d346b1c2258994fa265cdf98589cd86
= <=>
2021-11-17 16:18:10 +0100

    solutions for ex02 exercises completed today

--- exercises/ex02/solutions.py
+++ exercises/ex02/solutions.py
@@ -1,5 +1,6 @@
 import json
 import csv
+import os
 
 # Question 1 #################################################################
 
@@ -90,9 +91,21 @@ def test_json_data(part_path, key=None):
     return d1 == d2
 
 # Question 4 #################################################################
-# to be done next time
+def data2txt(data): # data in tsv2list format
+    names = data[0].keys() # get column names
+    # build a dictionary with column names as keys, lengths as values
+    lendict = {}
+    for name in names:
+        lendict[name] = max([len(str(d[name])) for d in data])
+    for d in data:
+        string = ""
+        for name in names:
+            chars = str(d[name])
+            spaces = (2 + (lendict[name] - len(chars))) * " "
+            string += chars + spaces
+        print(string)
 
-# Question 5 (partial solution, the complete one will be provided next time) #
+# Question 5 #################################################################
 
 def n_countries(d):
     return len(d)
@@ -107,12 +120,47 @@ def most_common_curr(d):
     currlist = sorted([(count,name) for (name,count) in currdict.items()])
     return currlist[-1]
 
+def least_diff_in_pop(d):
+    pop_name_list = sorted(
+        [(d['population'],name) for (name, d) in d.items()])
+    c1 = pop_name_list[0][1]
+    c2 = pop_name_list[1][1]
+    min_diff = pop_name_list[1][0] - pop_name_list[0][0]
+    for (i, (pop, name)) in enumerate(pop_name_list[:-1]):
+        (next_pop, next_name) = pop_name_list[i + 1]
+        diff = next_pop - pop  
+        if diff < min_diff:
+            min_diff = diff
+            c1 = name
+            c2 = next_name
+    return c1, c2
+
+def countries_by_density(d):
+    return sorted(
+        [(d['population'] / d['area'] ,name) for (name, d) in d.items()], 
+        reverse=True)
+
 def query_test():
     d = tsv2dict('countries.tsv', key='country')
     print("How many countries are there?")
     print(n_countries(d))
     print("What is the most common name of a currency?")
     print(most_common_curr(d))
+    print("Which two countries have the smallest difference in population?")
+    print(least_diff_in_pop(d))
+    print("List the 20 countries with the highest population density" 
+    + " population divided by area), together with the densities, in a " 
+    + "descending order of density.")
+    print(countries_by_density(d)[:20])
 
 # Question 6 #################################################################
-# to be done next time
+def dir2dict(path='.'):
+    if os.path.isfile(path):
+        return None
+    else: # os.path.isdir(path)
+        d = {}
+        d[os.path.basename(path)] = {}
+        for el in os.listdir(path):
+            d[os.path.basename(path)][el] = dir2dict(os.path.join(path,el))
+            print(d)
+        return d
