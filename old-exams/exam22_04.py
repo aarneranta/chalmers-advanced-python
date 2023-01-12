b25e85f7c5763060031b083b7c4782863b793cd8
aarneranta <aarne@chalmers.se>
2023-01-12 12:02:15 +0100

    not necessary to define __str__ in April exam q6

--- old-exams/exam22_04.py
+++ old-exams/exam22_04.py
@@ -77,25 +77,17 @@ class Vehicle:
     def get_brand(self):
         return self._brand
 
-    def __str__(self):
-        return 'brand: ' + self._brand
-
 
 class Car(Vehicle):
     def __init__(self, brand, acceleration):
         Vehicle.__init__(self, brand)
         self._acceleration = acceleration
 
-    def __str__(self):
-        return super().__str__() + ', acceleration:' + str(self._acceleration)
-
+        
 class Electric(Vehicle):
     def __init__(self, brand, range):
         Vehicle.__init__(self, brand)
         self._range = range
-
-    def __str__(self):
-        return super().__str__() + ', range:' + str(self._range)
 
 
 class ElectricCar(Car, Electric):
