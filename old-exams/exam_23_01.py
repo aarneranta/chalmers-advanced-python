b14b406a6b906fd1f66f6ff96492e6e585c3d284
aarneranta <aarne@chalmers.se>
2023-01-18 11:54:34 +0100

    some explanations and alternative solutions for the exam

--- old-exams/exam_23_01.py
+++ old-exams/exam_23_01.py
@@ -1,8 +1,12 @@
 # 1
 
 """
+1.5 points per question. The total sum is rounded upwards: 7.5 gives 8
+
 'madam'.reverse()
 AttributeError: : 'str' object has no attribute 'reverse'
+TypeError also accepted
+value None is not accepted
 
 list(range(1, 10, 2))
 [1, 3, 5, 7, 9]
@@ -15,6 +19,7 @@ TypeError: unhashable type: 'set'
 
 len({print(n) for n in range(5)})
 1
+If the solution shows the printing, it is OK, but it must be clear that 1 is the value.
 
 {1, 2, 1} != {1, 2}
 False
@@ -30,6 +35,7 @@ False
 # 2
 
 e = lambda xs, ys: set(xs + ys)
+# it is not necessary to include "e = ": the lambda expression itself is enough
 
 # 3
 
@@ -45,6 +51,7 @@ q3a = {c for c in countries if countries[c]['continent'] != 'Europe'}
 q3b = len({c for c in countries if countries[c]['continent'] != 'Europe' and countries[c]['currency'] == 'euro'})
 # also OK to use the first expression as a part
 q3b2 = len({c for c in q3a if countries[c]['currency'] == 'euro'})
+# in both cases, it is also OK to use lists rather than sets, because dict keys are unique
 
 # 4
 
@@ -56,11 +63,13 @@ adj_g = {
     5: {1, 6},
     6: {2, 3, 4, 5}
     }
-
+# it is OK for the values to be lists
 
 # 5
 # the example is from https://realpython.com/python-super/#an-overview-of-pythons-super-function
-
+# 3p for the class definition, 3p in total for the use examples
+    
+# the class Rectangle was given
 class Rectangle:
     def __init__(self, length, width):
         self.length = length
@@ -73,9 +82,16 @@ class Rectangle:
         return 2 * self.length + 2 * self.width
 
 
+# the class Square is to be defined
 class Square(Rectangle):
     def __init__(self, side):
         super().__init__(side, side)
+        
+        # also Rectangle.__init__(side, side) is OK
+        # and so is explicitly defining length and width as side,
+        # because __init__() has to be overwritten
+# The important thing is that area() and perimeter() are not redefined but inherited
+
 
 R = Rectangle(3, 2)
 
@@ -90,6 +106,7 @@ S.area()
 # bonus colouring
 
 # order: 7, 3, 4, 5, 1, 6
+# many other orders are also valid, as long as the removed vertices always have less than 3 edges
 
 # colours: 6 R, 1 R, 5 B, 4 B, 3 B, 2 B
 # in fact two colours are enough, but the simplify-select algorithm cannot get started with 2
