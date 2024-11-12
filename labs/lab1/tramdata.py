11b5fa1002f2ba918dfffcd058734eb64d07bfec
felixarv <felixarv@chalmers.se>
2024-11-12 11:19:34 +0100

    Update tramdata.py

--- labs/lab1/tramdata.py
+++ labs/lab1/tramdata.py
@@ -1,8 +1,8 @@
 import json
 
-TRAM_STOP_FILE = "/../data/tramstops.json"
-with open(TRAM_STOP_FILE, 'r') as file:
-    data = json.load(file)
+TRAM_STOP_FILE = "./../data/tramstops.json"
+with open(TRAM_STOP_FILE, 'r') as fromFile:
+    data = json.load(fromFile)
 
 
 def build_tram_lines(lines):
