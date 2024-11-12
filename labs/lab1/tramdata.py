44d12707ef919afa74348bff2fc9c5886ee3ca90
filiplarsen231 <filiplarsen231@gmail.com>
2024-11-12 13:59:20 +0100

    Tramstops fixad
    lagt till funktion

--- labs/lab1/tramdata.py
+++ labs/lab1/tramdata.py
@@ -1,12 +1,18 @@
 import json 
 
-TRAM_STOP_FILE = "./../data/tramstops.json"
+TRAM_STOP_FILE = 'labs/data/tramstops.json'
 with open(TRAM_STOP_FILE, 'r') as fromFile:
     data = json.load(fromFile)
 
+#print(json.dumps(data, indent=2, ensure_ascii=False))
 
 def build_tram_stops(jsonobject):
-    pass
+
+    stopdict = {x: {"lat":jsonobject[x]['position'][0], "lon":jsonobject[x]['position'][1]} for x in jsonobject}
+
+    return stopdict
+
+print(build_tram_stops(data))
 
 def build_tram_lines(lines):
     pass
