26caec0aeb5d25fee80c31e9b2bd9b8bc82db005
filiplarsen231 <filiplarsen231@gmail.com>
2024-11-12 10:53:50 +0100

    Create tramdata.py

---
+++ labs/lab1/tramdata.py
@@ -0,0 +1,25 @@
+import json
+
+def build_tram_lines(lines):
+    pass
+
+def build_tram_stops(jsonobject):
+    pass
+
+def build_tram_network(stopfile, linefile):
+    pass
+
+def lines_via_stop(linedict, stop):
+    pass
+
+def lines_between_stops(linedict, stop1, stop2):
+    pass
+
+def time_between_stops(linedict, timedict, line, stop1, stop2):
+    pass
+
+def distance_between_stops(stopdict, stop1, stop2):
+    pass
+
+def dialogue(tramfile):
+    pass
