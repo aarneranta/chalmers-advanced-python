a29849a300d3fb2e5aff3028f04591fc1aa28e39
Aarne Ranta <aarne@chalmers.se>
2024-12-13 15:32:04 +0100

    changing urls in the svg file, for lab3

---
+++ labs/lab3/files/change_urls.py
@@ -0,0 +1,38 @@
+import json
+
+# quick and dirty way to change links in our SVG IN_FILE
+
+STOPID_FILE = 'stopids.json'  # your generated file, see lecture 7
+
+
+GOOGLE_FILE = 'tramstop_google_url.json'  # in lab3/files/ directory
+SVG_IN_FILE = 'gbg_tramnet.svg'  # in lab3/files/
+
+GOOGLE_PREFIX = 'https://www.google.com/'
+
+with open(GOOGLE_FILE) as file:
+    googles = json.load(file)
+    googles = {val: key for key, val in googles.items()}
+ 
+with open(STOPID_FILE) as file:
+    stopids = json.load(file)
+ 
+url_changes = {google: stopids[stop] for google, stop in googles.items()}
+
+
+
+# lines in SVG_IN_FILE to be changed have three parts
+def change_line(line, changes):
+    if (i := line.find(GOOGLE_PREFIX)) > 0:
+        old_url = line[i:].split()[0][:-1]
+        new_url = url_changes.get(old_url, 'foo').replace('&', '&amp;')
+        return line.replace(old_url, new_url)
+    else:
+        return line
+
+
+with open(SVG_IN_FILE) as file:
+    for line in file:
+        print(change_line(line, url_changes).strip())
+
+
